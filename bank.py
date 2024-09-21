c17471a95dad0d85e7e3034c56cb6fee5c2ef24d
Colin Swain <colinmswain@gmail.com>
2024-09-20 22:53:20 -0400

    final add

--- bank.py
+++ bank.py
@@ -1,5 +1,3 @@
-#todo: List of multiplle accounts, pass through to withdraw and dpeosit threads, with 3 threads per account
-# implement a sync primitive
 #Colin Swain and Marcus Bollacker
 import threading
 import time
@@ -7,20 +5,40 @@ import time
 class BankAccount:
     def __init__(self, x):
         self.balance = x
+        self.lock = threading.Lock()
     def deposit(self, x):
-        if(x>0):
-            self.balance += x
-            print(f"{self.balance}")
-        else:
-            print("can only deposit values greater than 0")
+        with self.lock:
+            if(x>0):
+                self.balance += x
+                print(f"{self.balance}")
+            else:
+                print("can only deposit values greater than 0")
     def withdraw(self, y):
-        if self.balance>=y:
-            self.balance -= y
-            print(f"{self.balance}")
-        else:
-            print("not enought funds")
+        with self.lock:
+            if self.balance>=y:
+                self.balance -= y
+                print(f"{self.balance}")
+            else:
+                print("not enought funds")
     def amount(self) -> str:
         return self.balance
+
+    def depositTest(self):
+        count = 0
+        while count < 10000:
+            self.deposit(2)
+            count +=1
+
+    def withdrawTest(self):
+        count = 0
+        while count < 10000:
+            self.withdraw(1)
+            count +=1
+       
+            
+
+
+#intitalizes with an account and amount being deposited/withdrawn
 
 class DepositThread(threading.Thread):
     def __init__(self, account, amount):
@@ -40,36 +58,88 @@ class WithdrawThread(threading.Thread):
         self.account.withdraw(self.amount)
 
 
+accounts = []
+count = 0
+while count <5:
+    accounts.append(BankAccount(100))
+    count+=1
 
-startThread = time.time()
-account = BankAccount(1000)
+depositThread = []
+withdrawThread = []
+# Create multiple deposit and withdrawal threads in lists for accounts
+for account in accounts:
+    depositThread.append(DepositThread(account, 5))
+    withdrawThread.append(WithdrawThread(account, 1))
 
-# Create multiple deposit and withdrawal threads
-deposit_threads = [DepositThread(account, 200) for _ in range(5)]
-withdrawal_threads = [WithdrawThread(account, 300) for _ in range(3)]
-
-# Start all threads
-for thread in deposit_threads + withdrawal_threads:
+startThread = time.time() #start timer for testing purposes
+for thread in depositThread + withdrawThread:
     thread.start()
 
-# Wait for all threads to finish
-for thread in deposit_threads + withdrawal_threads:
+for thread in depositThread + withdrawThread:
     thread.join()
 endThread = time.time()
 
-print(f"{endThread - startThread}")
-#print(f"{endThread - startThread} seconds elapsed \n Account1 Balance:{account1.balance} Account2 Balance: {account2.balance}") 
-#print("----------------------")
 
+print(f"Multithreaded: {endThread - startThread}") 
+
+print("press enter to continue")
+input()
+
+#Test efficency of multithreading vs sequential single thread one account
+account1 = BankAccount(100)
+depositThread2 = []
+withdrawThread2 = []
+count = 0
+
+while count < 10000:
+    depositThread2.append(DepositThread(account1, 5))
+    withdrawThread2.append(WithdrawThread(account1, 1))
+    count +=1
+
+ #start timer for testing purposes
+startThread2 = time.time()
+for thread in depositThread2 + withdrawThread2:
+    thread.start()
+
+for thread in depositThread2 + withdrawThread2:
+    thread.join()
+endThread2 = time.time()
 
 # accounts with sequential threading
-#startSequential = time.time()
-#account1.depositTest()
-#account2.depositTest()
-#endSequential = time.time()
+account2 = BankAccount(100)
+count = 0
+startSequential = time.time()
+while count <10000:
+    account2.deposit(5)
+    account2.withdraw(1)
+    count+=1
 
-#print(f"{endSequential - startSequential} seconds elapsed \n Account1 Balance:{account1.amount()} Account2 Balance: {account2.balance}") 
 
+endSequential = time.time()
 
+print(f"Sequential: {endSequential - startSequential} vs Multithreaded: {endThread2 - startThread2}") 
+print(f"Account2 Balance: {account2.amount()} vs Account1 Balance: {account1.amount()}") 
 
-    
+print("press enter to continue")
+input()
+
+# two  accounts thread test
+testAccount1 = BankAccount(100)
+testAccount2 = BankAccount(100)
+fread = threading.Thread(target = testAccount1.depositTest)
+dread = threading.Thread(target = testAccount1.withdrawTest)
+
+startMulti = time.time()
+fread.start()
+dread.start()
+fread.join()
+dread.join()
+endMulti = time.time()
+
+startSequential = time.time()
+testAccount2.depositTest()
+testAccount2.withdrawTest()
+endSequential = time.time()
+
+print(f"Sequential: {endSequential - startSequential} vs Multithreaded: {endMulti - startMulti}") 
+print(f" Account2 Balance: {testAccount2.amount()} vs Account1 Balance: {testAccount1.amount()}") 
