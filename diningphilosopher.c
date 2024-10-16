3e6628c0f70e5fb8e7bb7ff2bcccf7a5045b93de
Colin Swain <colinmswain@gmail.com>
2024-10-16 17:14:42 -0400

    randomness

--- diningphilosopher.c
+++ diningphilosopher.c
@@ -1,21 +1,30 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
 
+//if philsopher is thinking tick hunger - 1
 int thinking(int health){ 
     return health -1;
-
 }
+//if philfopher is eating tick hunger plus 1
 int eating(int health){ 
     return health+1;
 }
 main()
 {
+    srand(time(NULL));
     
     int philos[5][4];
     int fork[5][3];
     int i = 0;
     while(i<5) {
         philos[i][0] = i; //name
-        philos[i][1] = 100; //random number health
+        //random number health greater than 20
+        if(((rand()%101)>=20))
+            philos[i][1] = rand()%101;
+        else
+            philos[i][1] = 20; 
         philos[i][2] = 0; //acces to fork 1
         philos[i][3] = 0; //access to fork 2
         fork[i][0]=i; //fork name
@@ -39,9 +48,12 @@ main()
     for (i = 0; i < 5; i++) {
         printf("fork[%d]: %d %d %d\n", i, fork[i][0], fork[i][1], fork[i][2]);
     }
-    
+    //printf("File: %s, Line:  %d \n",__FILE__,__LINE__);
     //run loop while all philosphers are alive
     while(philos[0][1]>=0) {
+        //philsopdher is thinking unless otherwise stated
+        int h =  philos[0][1];
+        philos[0][1] = thinking(h);
         //if both forks that philosopher can access are available and health less than 20 then access fork 
         if(philos[0][1]<=20 && fork[0][2]==0 && fork[1][2]==0) {
             fork[0][2] = 1;
