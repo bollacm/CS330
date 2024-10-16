dbf65d08be2b8412ca8523c8b8fbb315a3b43969
bollacm <bollacm@sunypoly.edu>
2024-10-16 17:21:34 -0400

    added could eat functionality and synch console output

--- diningp.c
+++ diningp.c
@@ -5,8 +5,10 @@
 #include <unistd.h>
 
 int philosopher_hunger[5] = {100, 100, 100, 100, 100};
+int hunger_tick = 10;
 pthread_mutex_t forks[5];
 pthread_t philosophers[5];
+pthread_mutex_t console_mutex;
 
 //hungry
 bool is_hungry(int philosopher_hunger[5], int i) {
@@ -28,19 +30,30 @@ bool is_thinking(int philosopher_hunger[5], int i) {
 
 //eating
 void eating(int philosopher_hunger[5], int i) {
-    philosopher_hunger[i] += 10; // Increment hunger meter by 10
+    philosopher_hunger[i] += hunger_tick; // Increment hunger meter
     if (philosopher_hunger[i] > 100) {
         philosopher_hunger[i] = 100;
     }
 }
 
+bool could_eat(int i) {
+    // Check if any philosopher is hungry
+    for (int j = 0; j < 5; j++) {
+        if (is_hungry(philosopher_hunger, j)) {
+            return false; // Prioritize hungry philosophers
+        }
+    }
+    return philosopher_hunger[i] < 100; // Allow eating if hunger is less than 100
+}
+
 void* philosopher(void* num) {
     int i = *(int*)num;
+
     while (true) {
 
 
-        // Decrement hunger level by 10 for each tick
-        philosopher_hunger[i] -= 10;
+        // Decrement hunger level for each tick
+        philosopher_hunger[i] -= hunger_tick;
         if (philosopher_hunger[i] <= 0) {
             philosopher_hunger[i] = 0;
             break;
@@ -49,20 +62,43 @@ void* philosopher(void* num) {
 
         // Thinking
         if (is_thinking(philosopher_hunger, i)) {
+            pthread_mutex_lock(&console_mutex);
             printf("Philosopher %d is thinking. Hunger: %d \n", i, philosopher_hunger[i]);
+            pthread_mutex_unlock(&console_mutex);
             sleep(1);
         }
 
-        // Hungry
-        if (is_hungry(philosopher_hunger, i)) {
-            printf("Philosopher %d is hungry.  Hunger: %d\n", i, philosopher_hunger[i]);
+        // Hungry or wants to eat
+        if (could_eat(i)) {
+            pthread_mutex_lock(&console_mutex);
+             if (is_hungry(philosopher_hunger, i)){
+                printf("Philosopher %d is hungry.  Hunger: %d\n", i, philosopher_hunger[i]);
+             } else {
+                printf("Philosopher %d wants to eat. Hunger: %d\n", i, philosopher_hunger[i]);
+            }
+    
+
+            pthread_mutex_unlock(&console_mutex);
 
             // Pick up forks
             pthread_mutex_lock(&forks[i]);
             pthread_mutex_lock(&forks[(i + 1) % 5]);
 
+            // Check if philosopher is dead
+            if (philosopher_hunger[i] == 0) {
+                pthread_mutex_lock(&console_mutex);
+                printf("Philosopher %d has died while holding forks.\n", i);
+                pthread_mutex_unlock(&console_mutex);
+                // Do not release forks, causing a deadlock
+                while (true) {
+                    sleep(1); // Infinite loop to simulate deadlock
+                }
+            }
+
             // Eating
+            pthread_mutex_lock(&console_mutex);
             printf("Philosopher %d is eating.  Hunger: %d \n", i, philosopher_hunger[i]);
+            pthread_mutex_unlock(&console_mutex);
             eating(philosopher_hunger, i);
             sleep(1);
 
@@ -78,12 +114,11 @@ void* philosopher(void* num) {
 
 int main() {
 
-    //int philosopher_hunger[5] = {100, 100, 100, 100, 100};
-    //bool fork[5] = {true, true, true, true, true};
-      // Initialize mutexes
+    // Initialize mutexes
     for (int i = 0; i < 5; i++) {
         pthread_mutex_init(&forks[i], NULL);
     }
+    pthread_mutex_init(&console_mutex, NULL);
 
     // Create philosopher threads
     int philosopher_ids[5];
@@ -101,6 +136,8 @@ int main() {
     for (int i = 0; i < 5; i++) {
         pthread_mutex_destroy(&forks[i]);
     }
+    pthread_mutex_destroy(&console_mutex);
 
 
+    return 0;
 }
